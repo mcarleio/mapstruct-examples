2277fb5171645b47158ff797db747eb1707fd7a1
Marcel Carlé <hi-github@mcarle.io>
2022-02-15 18:48:15 +0100

    add value class to showcase mapstruct having problems with kotlin's value classes

--- mapstruct-kotlin/src/main/kotlin/org/mapstruct/example/kotlin/Main.kt
+++ mapstruct-kotlin/src/main/kotlin/org/mapstruct/example/kotlin/Main.kt
@@ -2,14 +2,22 @@ package org.mapstruct.example.kotlin
 
 import org.mapstruct.example.kotlin.converter.PersonConverter
 import org.mapstruct.example.kotlin.model.Person
+import org.mapstruct.example.kotlin.model.PersonId
 import org.mapstruct.factory.Mappers
 import java.time.LocalDate
+import java.util.*
 
 fun main(args: Array<String>) {
 
     val converter = Mappers.getMapper(PersonConverter::class.java) // or PersonConverterImpl()
 
-    val person = Person("Samuel", "Jackson", "0123 334466", LocalDate.of(1948, 12, 21))
+    val person = Person(
+        PersonId(UUID.randomUUID().toString()),
+        "Samuel",
+        "Jackson",
+        "0123 334466",
+        LocalDate.of(1948, 12, 21)
+    )
 
     val personDto = converter.convertToDto(person)
     println(personDto)
--- mapstruct-kotlin/src/main/kotlin/org/mapstruct/example/kotlin/model/Person.kt
+++ mapstruct-kotlin/src/main/kotlin/org/mapstruct/example/kotlin/model/Person.kt
@@ -2,4 +2,13 @@ package org.mapstruct.example.kotlin.model
 
 import java.time.LocalDate
 
-data class Person(var firstName: String?, var lastName: String?, var phoneNumber: String?, var birthdate: LocalDate?)
+data class Person(
+    var id: PersonId?,
+    var firstName: String?,
+    var lastName: String?,
+    var phoneNumber: String?,
+    var birthdate: LocalDate?
+)
+
+@JvmInline
+value class PersonId(val id: String)
